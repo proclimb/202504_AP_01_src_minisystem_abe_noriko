91df45781f28d77abaf2906d151628df54177ec8
Noriko Abe <202504-AP-01-noriko-abe>
2025-05-28 16:45:07 +0900

    input.php修正2

--- develop/input.php
+++ develop/input.php
@@ -10,26 +10,26 @@ if (!empty($_POST) && empty($_SESSION['input_data'])) {
 
     if (empty($_POST['kana'])) {
         $error_message['kana'] = 'ふりがなが入力されていません';
-    } elseif (preg_match('/[^ぁ-ん－]/u', $_post['kana'])) {
+    } elseif (preg_match('/[^ぁ-ん－]/u', $_POST['kana'])) {
         $error_message['kana'] = 'ひらがなを入れて下さい';
     }
     $reg_ste = "/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@[A-Za-z0-9_-]+.[A-Za-z0-9]+$/";
 
     if (empty(['email'])) {
         $error_message['email'] = 'メールアドレスが入力されていません';
-    } elseif (!preg_match($reg_ste, $post['email'])) {
+    } elseif (!preg_match($reg_ste, $POST['email'])) {
         $error_message['email'] = 'メールアドレスが正しくありません';
     }
 
     $reg_str = "/^[0-9]{2,4}-[0-9]{2,4}-[0-9]{3,4}$/";
     if (empty($_post['tel'])) {
         $error_message['tel'] = '電話番号が入力されていません';
-    } elseif (!preg_match($reg_str, $_post['tel'])) {
+    } elseif (!preg_match($reg_str, $_POST['tel'])) {
         $error_message['tel'] = '電話番号が違います';
     }
 
     if (empty($error_message)) {
-        $_SESSION['input_data'] = $_post;
+        $_SESSION['input_data'] = $_POST;
         header('Location:confirm.php');
         exit();
     }
@@ -49,14 +49,14 @@ session_destroy();
 
 <body>
     <div>
-        <h1>mini system</h1>
+        <h1>mini System</h1>
     </div>
     <div>
         <h2>登録画面</h2>
     </div>
     <div>
         <form action="input.php" method="post" name="form">
-            <h1>class="constant_title">登録内容入力</h1>
+            <h1 class="contact-title">登録内容入力</h1>
             <p>登録内容をご入力の上、「確認画面へ」ボタンをクリックしてください。</p>
             <div>
                 <div>
@@ -76,36 +76,37 @@ session_destroy();
                 <div>
                     <label>メールアドレス<span>必須</span></label>
                     <input type="text" name="email" placeholder="例) guest@example.com" value="<?php echo $_POST['email'] ?>">
-                    <?php if (iss($error_message['email'])) { ?>
+                    <?php if (isset($error_message['email'])) { ?>
                         <div class="error-msg"><?php echo $error_message['email'] ?></div>
                     <?php } ?>
                 </div>
                 <div>
                     <label>電話番号<span>必須</span></label>
-                    <input type="text" name="tel" placeholder="例) 000-000-0000" value="<?php echo $_post['tel'] ?>">
+                    <input type="text" name="tel" placeholder="例) 000-000-0000" value="<?php echo $_POST['tel'] ?>">
                     <?php if (isset($error_message['tel'])) { ?>
                         <div class="error_msg"><?php echo $error_message['tel'] ?></div>
                     <?php } ?>
                 </div>
-                <label>性別<span>必須</span></label>]
-                <?php if (empty($_post['gender'])) { ?>
-                    <input type="radio" name="gender" value="男性" checked> 男性
-                    <input type="radio" name="gender" value="女性"> 女性
-                <?php } ?>
-                <?php if ($post['gender'] == "男性") { ?>
-                    <input type="radio" name="gender" value="男性" checked> 男性
-                    <input type="radio" name="gender" value="女性"> 女性
-                <?php } elseif ($_post['gender'] == "女性") { ?>
-                    <input type="radio" name="gender" value="男性"> 男性
-                    <input type="radio" name="gender" value="女性" checked> 女性
-                <?php } ?>
+                <div>
+                    <label>性別<span>必須</span></label>
+                    <?php if (empty($_POST['gender'])) { ?>
+                        <input type="radio" name="gender" value="男性" checked> 男性
+                        <input type="radio" name="gender" value="女性"> 女性
+                    <?php } ?>
+                    <?php if ($_POST['gender'] == "男性") { ?>
+                        <input type="radio" name="gender" value="男性" checked> 男性
+                        <input type="radio" name="gender" value="女性"> 女性
+                    <?php } elseif ($_POST['gender'] == "女性") { ?>
+                        <input type="radio" name="gender" value="男性"> 男性
+                        <input type="radio" name="gender" value="女性" checked> 女性
+                    <?php } ?>
+                </div>
             </div>
-    </div>
-    <button type="submit">確認画面へ</button>
-    <a href="index.php">
-        <button type="button">TOPに戻る</button>
-    </a>
-    </form>
+            <button type="submit">確認画面へ</button>
+            <a href="index.php">
+                <button type="button">TOPに戻る</button>
+            </a>
+        </form>
     </div>
 </body>
 
